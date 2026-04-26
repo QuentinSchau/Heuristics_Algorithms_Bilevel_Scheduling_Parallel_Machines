1253af696fbec0e066997599efb382e4d4f36b55
schau <quentin.schau@univ-tours.fr>
2026-04-26 14:21:50 +0200

    update parameter for automatic settings

--- scripts/generateInstances.py
+++ scripts/generateInstances.py
@@ -158,7 +158,6 @@ def main(args):
                             ]
                         })
                     elif method == "BeamSearch":
-                        {"alpha1": -0.08314, "alpha2": 7.58913, "alpha3": 0.02676, "alpha4": 4.0, "alpha5": 1460.0, "alpha6": 0.3765659378381789}
                         m = (mMax + M0)
                         W = math.floor(math.exp(-0.08314*N+7.58913) + 0.02676 * m) + 4 if (nbSolutionForMSLS > 0) else math.floor(math.exp(-0.2*N+4.40793) + 0.38714 * m ) + 5
                         K = 1460
